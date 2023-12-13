e588425b0aa1c4f37a870914261fdd701110b66d
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-12-13 22:13:38 +0700

    Added fragments and on click listeners

--- app/src/main/java/com/example/heralert/HomePage.java
+++ app/src/main/java/com/example/heralert/HomePage.java
@@ -1,14 +1,54 @@
 package com.example.heralert;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.heralert.databinding.ActivityHomePageBinding;
+import com.example.heralert.databinding.ActivityMainBinding;
 
 public class HomePage extends AppCompatActivity {
 
+    ActivityHomePageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item-> {
+
+            switch (item.getItemId()){
+                case R.id.contact:
+                    replaceFragment(new ContactsFragment());
+                    break;
+
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+
+            }
+            return true;
+        });
+
+        binding.floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new HomeFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
