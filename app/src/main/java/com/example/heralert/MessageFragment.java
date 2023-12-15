c8131df781aa7f00af795fd1f365aa6d5e33b57f
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-12-15 21:11:26 +0700

    Message Page 10%(ish) finished :)

--- app/src/main/java/com/example/heralert/MessageFragment.java
+++ app/src/main/java/com/example/heralert/MessageFragment.java
@@ -3,6 +3,7 @@ package com.example.heralert;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,21 +11,41 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MessageFragment extends Fragment {
 
     AppCompatButton backBtn;
+    SwitchCompat autoSendSwitch, sosSoundSwitch;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_message, container, false);
         backBtn = v.findViewById(R.id.backBtn);
+        autoSendSwitch = v.findViewById(R.id.autoSendSwitch);
+        sosSoundSwitch = v.findViewById(R.id.sosSoundSwitch);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 replaceFragment(new HomeFragment());
             }
         });
+
+        autoSendSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAutoSendClick(v);
+            }
+        });
+
+        sosSoundSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSosSwitchClick(v);
+            }
+        });
+
         // Inflate the layout for this fragment
         return v;
     }
@@ -34,5 +55,23 @@ public class MessageFragment extends Fragment {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    public void onAutoSendClick(View view){
+        if(autoSendSwitch.isChecked()){
+
+        }
+        else{
+
+        }
+    }
+
+    public void onSosSwitchClick(View view){
+        if(sosSoundSwitch.isChecked()){
+
+        }
+        else{
+
+        }
     }
 }
