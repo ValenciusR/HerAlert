a29062fb213339168b737f8f2c94ac805d41086a
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-12-15 15:33:24 +0700

    Added back button and Page Title to every page  other than home  page

--- app/src/main/java/com/example/heralert/ContactsFragment.java
+++ app/src/main/java/com/example/heralert/ContactsFragment.java
@@ -2,18 +2,36 @@ package com.example.heralert;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ContactsFragment extends Fragment {
-
+    AppCompatButton backBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_contacts, container, false);
+        backBtn = v.findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new HomeFragment());
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        return v;
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/heralert/SettingsFragment.java
+++ app/src/main/java/com/example/heralert/SettingsFragment.java
@@ -27,7 +27,7 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
-        
+
         backBtn = v.findViewById(R.id.backBtn);
         profileSelect = v.findViewById(R.id.profileSelect);
         messageSelect = v.findViewById(R.id.messageSelect);
