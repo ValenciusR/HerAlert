012db09a21332d736d9c5eefb6dbbc5db9cfa792
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-12-23 18:44:43 +0700

    Removed auto send message in message fragment

--- app/src/main/java/com/example/heralert/MessageFragment.java
+++ app/src/main/java/com/example/heralert/MessageFragment.java
@@ -19,7 +19,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class MessageFragment extends Fragment {
 
     AppCompatButton backBtn, updateBtn;
-    SwitchCompat autoSendSwitch, sosSoundSwitch;
+    SwitchCompat sosSoundSwitch;
     TextInputEditText helpMessageInputText, safeMessageInputText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,19 +29,11 @@ public class MessageFragment extends Fragment {
         updateBtn = v.findViewById(R.id.updateBtn);
         helpMessageInputText = v.findViewById(R.id.helpMessageInputText);
         safeMessageInputText = v.findViewById(R.id.helpMessageInputText);
-        autoSendSwitch = v.findViewById(R.id.autoSendSwitch);
         sosSoundSwitch = v.findViewById(R.id.sosSoundSwitch);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 replaceFragment(new SettingsFragment());
-            }
-        });
-
-        autoSendSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onAutoSendClick(v);
             }
         });
 
@@ -66,15 +58,6 @@ public class MessageFragment extends Fragment {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
-    }
-
-    public void onAutoSendClick(View view){
-        if(autoSendSwitch.isChecked()){
-
-        }
-        else{
-
-        }
     }
 
     public void onSosSwitchClick(View view){
