be7247a86fa7d4d1a1f91cd20537436921297e56
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-12-24 20:08:33 +0700

    changed home fragment a bit

--- app/src/main/java/com/example/heralert/HomeFragment.java
+++ app/src/main/java/com/example/heralert/HomeFragment.java
@@ -2,18 +2,31 @@ package com.example.heralert;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
 
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    AppCompatTextView helloUserTxt;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // ni buat textview hello user ming (figo)
+        helloUserTxt = v.findViewById(R.id.helloUserTxt);
+//        helloUserTxt.append(user.getEmail());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        return v;
     }
 }
